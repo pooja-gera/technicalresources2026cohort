fe6e034d86be63de710669d090f27faca5ceccc7
pooja-gera <55778515+pooja-gera@users.noreply.github.com>
2023-01-10 20:45:44 +0530

    Submitting Q1 of Assignment1

---
+++ a1_q1.java
@@ -0,0 +1,11 @@
+public class a1_q1 {
+    public static void main(String[] args) {
+        // Write your code here
+        System.out.println("*****");
+        System.out.println("   * ");
+        System.out.println("  *  ");
+        System.out.println(" *   ");
+        System.out.println("*****");
+        
+      }
+}
